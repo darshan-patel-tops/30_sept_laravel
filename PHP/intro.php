f837aed6121cbe81d6bce65aa34ea17853d6e3c3
darshan <darshan.patel.tops@gmail.com>
2023-09-30 13:12:04 +0530

    update

---
+++ PHP/intro.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+echo "welcome everyone ";
+echo " Vaibhavi ";
+echo " ankit ";
+
+
+
+?>
+
+<!-- PHP Supports HTML but HTML Does not support php -->
+
+<h1>hello welcome to your 1st lecture of php</h1>
+hello welcome to your 1st lecture of php
