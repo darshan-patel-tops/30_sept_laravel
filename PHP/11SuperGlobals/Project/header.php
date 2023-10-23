216e0697d110c0d5d63c13f8e59455e06321f712
darshan <darshan.patel.tops@gmail.com>
2023-10-23 12:50:52 +0530

    Update

--- PHP/11SuperGlobals/Project/header.php
+++ PHP/11SuperGlobals/Project/header.php
@@ -15,10 +15,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="home.php">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link active" aria-current="page" href="cart.php">Cart</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="productupload.php">Product Upload</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -48,9 +51,7 @@
 
           </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
+        
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
